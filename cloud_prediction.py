fffc1c0868e320d3385ad965596dd8bc56290bce
Coturex <coturex@gmail.com>
2022-02-16 16:52:19 +0100

    cloud average

--- cloud_prediction.py
+++ cloud_prediction.py
@@ -31,101 +31,103 @@ import requests
 import time
 import datetime
 from pprint import pprint
-
-
-from enum import Enum
-class Cloud(Enum):
-    TODAY = 0
-    DEMAIN = 1
-    TOMORROW = 1
-    DAY0 = 0
-    DAY1 = 1
-    DAY2 = 2
-    DAY3 = 3
-
+from debug import debug as debug
+ 
+TODAY = 0 
+DEMAIN = 1
+TOMORROW = 1
+DAY0 = 0
+DAY1 = 1
+DAY2 = 2
+DAY3 = 3
 class Prediction:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, location):
+        self.location = location
         self.energy = 0
         self.data = 0
 
-    def getApiKey():
+    def getApiKey(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
         return config['openweathermap']['api']
+        
+    def setLocation(self, location):
+        """Set OpenweatherMap location"""
+        self.location = location
 
-    def fetchData(self, type):
-        """Fetch data predictions, return value at UTC Time"""
+    def getCloudAvg(self, sDAY):
+        """ Return percent Average of clouds, @ UTC 09:00:00, UTC 12:00:00, UTC 13:00:00"""
+        """ Average : ((time1+time2+time3)/3)"""
+        s = 0
+        q = 3
+        c1 = self.getCloudHour(sDAY,9)
+        c2 = self.getCloudHour(sDAY,12)
+        c3 = self.getCloudHour(sDAY,15)
+        if (c1 <0):
+            q = q - 1
+        else:
+            s = s + c1
+        if (c2 <0):
+            q = q - 1
+        else:
+            s = s + c2
+        if (c3 <0):
+                q = q - 1
+        else:
+            s = s + c3
+        if q <= 0:
+            return -1
+        else:
+            avg = s/q
+            debug(1, "Cloud_prediction, Average : " +  str(avg))
+            return (avg)        
 
-    def getCloudAvg(DAY):
-        """ Return % Average of clouds, @ UTC 09:00:00, UTC 12:00:00, UTC 13:00:00"""
-        pass
+    def getRawData(self):
+        """Print JSON data returned by html request"""
+        try:
+            url = "https://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid={}".format(self.location, self.getApiKey())
+            wdata = requests.get(url).json()
+            pprint(wdata)
+        except:
+            return -1
 
-    def getCloudHour(DAY,H):
-        """ Return % of clouds at specific Hour, DAY"""
-        pass
-
-    def saveEnergy(self):
-        pass
-    
-    def reset_energy(self):
-        pass
+    def getCloudHour(self, sDAY, sHour):
+        """ Return percent of clouds at specific Hour, DAY"""
+        try:
+            sdate = datetime.date.today() + datetime.timedelta(days=sDAY)
+            sdate = str(datetime.datetime(sdate.year, sdate.month,sdate.day, sHour, 0, 0))
+            url = "https://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid={}".format(self.location, self.getApiKey())
+            #debug(0, "Cloud_prediction, url : " + url)
+            wdata = requests.get(url).json()
+            #pprint(wdata)
+            debug(0, "Cloud_prediction, Request : " + sdate)
+            for i in range(0,len(wdata['list'])):
+                datei  = wdata['list'][i]['dt_txt']
+                cloudi = wdata['list'][i]['clouds']['all']
+                #print(i, datei, cloudi, sdate)
+                if sdate == datei:  
+                    #print(i, datei, sdate, cloudi)
+                    debug(1, "Cloud_prediction, Clouds : " + str(cloudi))
+                    return cloudi
+        except Exception as e:
+            return -2
+        return -1     
 
     def log(self):
         """ Log cloud prediction, real cloud cover and real day energy production"""
         pass
 
-
-def get_weather(api_key, location):
-    url = "https://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid={}.format(location, api_key)"
-    r = requests.get(url)
-    return r.json()
-
 def main():
-
     #if len(sys.argv) != 2:
     #    exit("Usage: {} LOCATION".format(sys.argv[0]))
     #location = sys.argv[1]
-
-    location = "Chambery"
-    api_key = get_api_key()
-
-    api_key = ""
-    weather = get_weather(api_key, location)
+       
+    weather = Prediction("Chambery")
+    weather.getCloudHour(TODAY,18)
+    print(weather.getCloudAvg(TOMORROW))
+    #weather.getRawData()
    
-    for i in range(0,len(weather['list'])):
-        print(weather['list'][i]['dt_txt'])
-        print(weather['list'][i]['dt'])
-        print(weather['list'][i]['clouds']['all'])
-
-    tomorrow =  datetime.date.today() + datetime.timedelta(days=1)
-    dt = datetime.datetime(tomorrow.year, tomorrow.month,tomorrow.day, 0, 0, 0)
-    print ("tomorrow" , tomorrow)
-    ts = int(dt.timestamp())
-    print(ts)
-
-   
-
-# https://pymotw.com/3/datetime/
-
- 
-
-#    date_string = "2/11/2021, 04:5:8"
-
-#    date = datetime.datetime.strptime(date_string, "%m/%d/%Y, %H:%M:%S")
-
-#    time_stamp = datetime.datetime.timestamp(date)
-
-#    print(time_stamp)
-
-#    ts1 = int(
-
-    #print ts1
-
-    #pprint(weather)
-
 if __name__ == '__main__':
-
     main()
 
  
