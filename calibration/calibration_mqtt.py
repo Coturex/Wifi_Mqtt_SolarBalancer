0e6f42fa739fac395b52c6563896bfd56f36f51b
Coturex <coturex@gmail.com>
2022-10-11 18:36:19 +0200

    ccx

--- calibration/calibration_mqtt.py
+++ calibration/calibration_mqtt.py
@@ -11,14 +11,15 @@ import time
 import sys
 DEBUG = 0
 
+csv_file = open("out.csv", "w")
+log_file = open("out.log", "w")
 mqtt_client = None
-csv_file = None
-log_file = None
+
 percent = None   # Fixe la Valeur en Pourcentage [0..100] du variateur de puissance
 measuring = 0    # machine d'état
 avg_count = 0    # Comptage du nbre de mesure
 avg_power = 0    # moyenne (non pondérée)
-avg_samples = 12 # Nbre de mesures pour faire la moyenne des puissances
+avg_samples = 12  # Nbre de mesures pour faire la moyenne des puissances
 end_power = 100    # Puissance de l'ECS après l'execution du pgm 
 step_stabilization = 10   # attente stabilisation en secondes de la puissance entre chaque seuil
 start_stabilization = 500 # attente premiere stabilisation à l'allumage de l'ECS à pleine puissance (environ 8 minutes)
@@ -32,7 +33,7 @@ def on_connect(mqtt_client, userdata, flags, rc):
     print('Connected to mqtt ' + MQTT_BROKER)
 
 def on_pzem_message(client, userdata, msg):
-    global percent, avg_power, avg_count, measuring
+    global percent, avg_power, avg_count, measuring, csv_file, out_file
 
     if DEBUG : print("[on_pzem_message] avg_count,measuring,percent[" 
                     + str(avg_count) + "," + str(measuring)+ "," + str(percent)+"]")
@@ -52,7 +53,7 @@ def on_pzem_message(client, userdata, msg):
             if read_power > 0:
                 log = '[on_pzem_message] # read {} W'.format(read_power)
                 print (log)
-                log_file.write(log)            
+                log_file.write(log+"\n")            
                 sys.stdout.flush()
                 avg_power += read_power
                 avg_count += 1
@@ -63,7 +64,7 @@ def on_pzem_message(client, userdata, msg):
             line = '{};{}\n'.format(percent, avg_power)
             line = line.replace(".", ",")
             print("[on_pzem_message] csvline power,average : " + line)
-            log_file.write(line)
+            log_file.write(" # power,average : " + line)
             csv_file.write(line)
             sys.stdout.flush()
             avg_power = 0
@@ -71,12 +72,13 @@ def on_pzem_message(client, userdata, msg):
             measuring = 0
 
 def signal_handler(signal, frame):
+    global log_file, csv_file
     print ("\n!! Ctrl+C pressed !!") 
     print ("ECS Set power : 0 !") 
     mqtt_client.publish(TOPIC_SET_POWER, str("0"))
     time.sleep(5)
-    csv_file.close()
     log_file.close()
+    csv_file.close()
     print("Calibration results saved on out.csv") 
     print ("Bye.") 
     sys.exit(0)
@@ -92,19 +94,15 @@ def main():
     mqtt_client.connect(MQTT_BROKER, 1883, 120)
     mqtt_client.loop_start()
     time.sleep(10)
-
-    csv_file = open("out.csv", "w")
-    log_file = open("out.log", "w")
-    
+   
     mqtt_client.publish(TOPIC_SET_POWER, str("100"))
     print ("Waiting for stabilization, " + str(start_stabilization)+ " secondes...")
     time.sleep(start_stabilization)   # Attente Premiere stabilisation d'environ 8 minutes
 
     for percent in np.arange(100, -0.5, -0.5):
-        print(percent)  
-        log ='# set power to {}%'.format(percent)
-        print (log)
-        
+        log ='\n# set power to {}%'.format(percent)
+        print(log)  
+        log_file.write(log + "\n")
         mqtt_client.publish(TOPIC_SET_POWER, str(percent))
         time.sleep(step_stabilization) # Attente 'Stabilisation' à chaque changement de puissance
         measuring = 1 
